f21210d6ae1f3f33ae8ec938805c81a3852245d5
darwin osingo <darwin.osingo@student.moringaschool.com>
2025-03-24 11:26:38 +0300

    I had not added an event to the form

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 // load stuff first!!
 document.addEventListener("DOMContentLoaded", function() {
-  
+
   // grab form 
   const form = document.getElementById('create-task-form'); // this is the form
   const taskList = document.getElementById('tasks'); // this is where tasks go
-  
+
   // when u press submit button 
   form.addEventListener('submit', function(submitEvent) {
     submitEvent.preventDefault(); // finna stop page from reloading
-    
+
     // get the text u typed
     const inputThing = document.getElementById('new-task-description');
     const task = inputThing.value;
 
-
     if (task != "") {
       // make a new list item 
       const newListItem = document.createElement('li');
-      newListItem.innerText = task; // put the task here
 
+      // add task text separately to avoid breaking the test
+      const taskText = document.createTextNode(task);
+      newListItem.appendChild(taskText); // clean task text for test
+      
       // makes a delete button
       const deleteTheTask = document.createElement('button');
       deleteTheTask.innerText = "X";
@@ -37,10 +39,8 @@ document.addEventListener("DOMContentLoaded", function() {
       inputThing.value = "";
     } else {
       // forgot to type something 
-      alert("write something first!!!");
+      alert("yo, write something first!!!");
     }
   });
 
 });
-
-// 
